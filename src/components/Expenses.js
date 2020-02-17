de9bf549a89175d2fa17b3090b95e1b5ba08c44f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-17 13:10:22 -0500

    [+] Component > Expenses (Form): State Bugget 'useState' & Events 'onChange, onSubmit'

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Expenses = () => {
+
+    /** State: Podr[ia definirse como un objeto que contenga ambos valores] */
+    const [ stateDescription, setDescription ] = useState( '' ),        // Descripcion del Gasto
+          [ stateAmount, setAmount ] = useState( 0 );                   // Monto del Gasto
+
+    /** Evento que solicita la cita al hacer click en el formulario */
+    const submitAddExpense = ( event ) => {
+        event .preventDefault();        // Evita el envio por el QueryString por el metodo GET
+
+        console .log( 'Envia', stateDescription, stateAmount );
+
+    }
+
     return (
-        <form className="form-expenses">
+        <form 
+            className="form-expenses"
+            onSubmit={ submitAddExpense }
+        >
             <h3>Registra tu gasto</h3>
             <div className="field">
                 <label>Descripción del gasto</label>
@@ -10,6 +26,8 @@ const Expenses = () => {
                     type="text"
                     placeholder="Ej: Transporte"
                     className="u-full-width"
+                    value={ stateDescription }
+                    onChange={ event => setDescription( event .target .value ) }        // Actualiza el State al lanzar el evento usando un Callback
                 />
             </div>
             <div className="field">
@@ -18,6 +36,8 @@ const Expenses = () => {
                     type="number"
                     placeholder="Ej: 30"
                     className="u-full-width"
+                    value={ stateAmount }
+                    onChange={ event => setAmount( parseInt( event .target .value ) ) }             // Actualiza el State al lanzar el evento usando un Callback
                 />
             </div>
 
