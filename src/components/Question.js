b303a834fc13a14f4e4aebee8e10741e03f63a8b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-17 10:45:27 -0500

    [+] Component > Question (Form): State Bugget 'useState' & Events 'onChange, onSubmit'

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,34 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Question = () => {
+
+    /** State: Presupuesto */
+    const [ statusBudget, setBuget ] = useState( 0 );
+
+    /** Manejador de Cambios el State: Define Presupuesto (Cuando el usuario escribe en un campo del formulario) */
+    const handleChange = ( event ) => {
+        console .log( parseInt( event .target .value ) );
+        setBuget( event .target .value );
+    }
+
+    /** Evento que solicita la cita al hacer click en el formulario */
+    const submitAddBudget = ( event ) => {
+        event .preventDefault();        // Evita el envio por el QueryString por el metodo GET
+
+        console .log( 'Envia', statusBudget );
+    }
+
     return (
         <Fragment>
             <h2>Ingresa tú presupuesto</h2>
-            <form>
+            <form
+                onSubmit={ submitAddBudget }
+            >
                 <input 
                     type="number"
                     className="u-full-width"
                     placeholder="Ej: 300"
+                    onChange={ handleChange }
                 />
                 <button
                     type="submit"
